cefebb9d61db960488dadc30e8e492a21ad0abdf
Chip Weinberger <weinberger.c@gmail.com>
2023-03-13 18:57:22 -0700

    [Stack Mirror] make corrupt backtraces not happen nearly as often

--- components/esp_system/port/arch/xtensa/panic_arch.c
+++ components/esp_system/port/arch/xtensa/panic_arch.c
@@ -11,6 +11,7 @@
 #include "esp_debug_helpers.h"
 
 #include "esp_private/panic_internal.h"
+#include "esp_private/stack_mirror.h"
 #include "esp_private/panic_reason.h"
 #include "soc/soc.h"
 
@@ -467,5 +468,8 @@ void panic_print_backtrace(const void *f, int core)
 {
     XtExcFrame *xt_frame = (XtExcFrame *) f;
     esp_backtrace_frame_t frame = {.pc = xt_frame->pc, .sp = xt_frame->a1, .next_pc = xt_frame->a0, .exc_frame = xt_frame};
-    esp_backtrace_print_from_frame(100, &frame, true);
+    esp_err_t err = esp_backtrace_print_from_frame(100, &frame, true);
+    //if (err != ESP_OK) {
+        stack_mirror_print_backtrace(true);
+    //}
 }
